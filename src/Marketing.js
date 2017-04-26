cb2e901e2238e54d3af39443f1cb727dd937dbbe
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-04-26 06:59:52 +0200

    Added missing parts of Marketing block

--- src/Marketing.js
+++ src/Marketing.js
@@ -35,6 +35,47 @@ export default class Marketing extends React.Component {
                         <p><a className="btn btn-default" href="apply" role="button">Zu den Parteien »</a></p>
                     </Col>
                 </Row>
+
+                <hr className="featurette-divider" />
+
+                <Row className="featurette">
+                    <Col md={7}>
+                        <h2 className="featurette-heading">Warum machen wir das{'?'}</h2>
+                        <p className="lead">Menschen suchen nach einfachen Antworten. Die Welt ist aber nicht so einfach.
+                            Wir wollen die richtigen von den falschen Informationen unterscheiden.
+                            Und Populisten von den Politikern, denen wir wirklich vertrauen können.</p>
+                        <a href="apply" className="btn btn-primary pull-right">Mehr erfahren</a>
+                    </Col>
+                    <Col md={5}>
+                        <img className="featurette-image img-responsive center-block" alt="" src="/img/FragenUndAntworten.jpg" />
+                    </Col>
+                </Row>
+
+                <Row className="featurette">
+                    <Col md={5}>
+                        <img class="featurette-image img-responsive center-block" alt="" src="/img/Zusammenarbeit.jpg" />
+                    </Col>
+                    <Col md={7}>
+                        <h2 className="featurette-heading">Wie machen wir das{'?'}</h2>
+                        <p className="lead">Dieses System lebt von der Mitarbeit vieler interessierter Menschen,
+                            die unsere Demokratie stärken und dem sogenannten "Post-Faktischen" entgegen wirken wollen.
+                        </p>
+                        <a href="apply" className="btn btn-primary pull-right">Mehr erfahren</a>
+                    </Col>
+                </Row>
+
+                <Row className="featurette">
+                    <Col md={7}>
+                        <h2 className="featurette-heading">Wie kann ich mitmachen{'?'}</h2>
+                        <p className="lead">Interessiert, mitzumachen{'?'} Sehr gern!
+                            Egal wie viel Zeit zur Verfügung steht - wir haben auf jeden Fall die richtigen Aufgaben.
+                        </p>
+                        <a href="apply" className="btn btn-primary pull-right">Mehr erfahren</a>
+                    </Col>
+                    <Col md={5}>
+                        <img className="featurette-image img-responsive center-block" alt="" src="/img/volunteer.jpg" />
+                    </Col>
+                </Row>
             </div>
         )
     }
