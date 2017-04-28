497edc55ff431e1594decfcc6508c49dd0f8c709
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-04-28 05:59:17 +0200

    Show long names of parties

--- src/PartyRow.js
+++ src/PartyRow.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default class PartyRow extends React.Component {
     render() {
-        return <li>{this.props.content.name}</li>;
+        return <li>{(this.props.content.name ? (this.props.content.name + '-') : '')
+            + this.props.content.longName}</li>;
     }
 }
