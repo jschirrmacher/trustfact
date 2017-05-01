20ba8c0f5c5b2e2f6009b9628d41b02774106c45
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-05-01 15:55:28 +0200

    Removed unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter, Route } from 'react-router-dom'
-import { LinkContainer } from 'react-router-bootstrap'
 
 import NavigationBar from './NavigationBar'
 import StartPage from './StartPage'
